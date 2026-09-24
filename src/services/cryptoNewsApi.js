c19763a48f33e2c5fc169c0690d6dcba565587b0
Ashwini Kumar <146967126+ashwini-coder11@users.noreply.github.com>
2026-09-24 15:07:43 +0530

    removed leaked api key

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,9 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const cryptoNewsHeaders = {
-  'x-rapidapi-key': 'bda3c80e36msh2f0e885190e0ef6p15d559jsn82c3f6b45c2e',
-    'x-rapidapi-host': 'google-news13.p.rapidapi.com'}
-
 const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
 
 export const cryptoNewsApi = createApi({
